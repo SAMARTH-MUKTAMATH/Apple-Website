ad9eba988876abf65515410707235d500a6e16ac
SAMARTH-MUKTAMATH <samarthmise2025@gmail.com>
2025-10-20 19:23:49 +0530

    Model added with gsap and color toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+// Register plugins with correct capitalization
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
 function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main> 
   )
 }
